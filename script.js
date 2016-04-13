bf88cc9c5cc4e0bc935d19bf4679d9e4c6b3dc33
Emilia Kamrowska <emilia.kamrowska@o2.pl>
2016-04-13 15:37:44 +0200

    working on privacy-policy box

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ $('.accountChoice').click( function() {
     $('.choice').toggleClass('fa-circle-o fa-dot-circle-o');
 });
 
+//$('.infoHover').hover(function() {
+//    $('.IBAN').addClass('visible');
+//});
