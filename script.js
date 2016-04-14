f229fed4eab881e0c8ae5d4c6509056893e661f0
Emilia Kamrowska <emilia.kamrowska@o2.pl>
2016-04-14 13:18:09 +0200

    animations of info boxes

--- script.js
+++ script.js
@@ -4,29 +4,21 @@ $(document).ready(function () {
         $('.choice').toggleClass('fa-circle-o fa-dot-circle-o');
     });
 
-//$('.infoHover').hover(function() {
-//    $('.IBAN').addClass('visible');
-//});
-
-//$('.policy').click( function() {
-//    $('.policyBox').removeClass('unvisible');
-//    $('.policyBox').addClass('visible');
-//});
-//
-//$('.closeX').click(function() {
-//    $('.policyBox').removeClass('visible');
-//    $('.policyBox').addClass('unvisible');
-//});
-
-
-    $('.policy').click(function () {
-        $('.policyBox').toggleClass('unvisible visible');
-
+    $('.policyClick').click(function () {
+        $('.policyBox').removeClass('unvisible');
+        $('.policyBox').addClass('visible');
     });
 
-$('#submitButton').click(function(event) {
+    $('.closeX').click(function () {
+        $('.policyBox').toggleClass('visible unvisible');
+    });
 
-    event.preventDefault();
+    $('.info').hover(function () {
+        $('.IBAN').toggleClass('visible unvisible');
+    });
+
+    $('#submitButton').click(function (event) {
+        event.preventDefault();
         if (($('.firstName').val()) == '') {
             $('.fNameError').attr('style', 'visibility:visible');
             $('.firstName').addClass('red');
@@ -34,24 +26,13 @@ $('#submitButton').click(function(event) {
             $('.fNameError').removeAttr('style', 'visibility:visible');
             $('.firstName').removeClass('red');
         }
-
         if (($('.lastName').val()) == '') {
             $('.lNameError').attr('style', 'visibility:visible');
             $('.lastName').addClass('red');
         } else {
             $('.lNameError').removeAttr('style', 'visibility:visible');
             $('.lastName').removeClass('red');
-
-    }});
-
-
+        }
+    });
 
 });
-
-//$('.closePopUp').click(function()   {
-//    $('#PopUp-wrapper').attr('style', 'display:none');
-//});
-//
-//$('.logInButton').click(function()  {
-//    $('#PopUp-wrapper').removeAttr('style', 'display:none');
-//})
